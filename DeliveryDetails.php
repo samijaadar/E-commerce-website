58717ff11f2bac8951a08d019a0d65fc6416f2fd
sami jaadar <56087072+samijaadar@users.noreply.github.com>
2020-08-17 19:33:58 +0100

    Update DeliveryDetails.php

--- DeliveryDetails.php
+++ DeliveryDetails.php
@@ -51,3 +51,81 @@ extract($row);
     </div>
 </body>
 </html>
+<style>
+*{
+  margin: 0;
+  padding:0;
+  text-decoration: none;
+  list-style-type: none;
+}
+/*body*/
+
+.container-box {
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: space-between;
+
+  margin: 60px 40px;
+}
+
+
+.product-card{
+background: white;
+    margin: 10px;
+    width: 250px;
+    padding: 15px;
+    border-radius: 30px;
+    text-transform: uppercase;
+box-shadow: 0 6px 10px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19);
+}
+.product-pic{
+    height:230px;
+}
+.product-pic img{
+  max-height:200px;
+  width:auto;
+  height:auto;
+  max-width:250px
+}
+.product-title h3{
+font-size: 20px;
+margin-bottom: 4px;
+color: black;
+}
+.product-title h5{
+font-size: 18px;
+margin-bottom: 5%;
+color: #bbb;
+}
+
+
+.product-info{
+display: flex;
+align-items: center;
+margin-top:15%;
+}
+.product-title{
+margin-bottom:40px;
+max-width: 300px;
+height:68px;
+overflow:hidden;
+}
+.product-price{
+color: #2175f5;
+font-size: 26px;
+margin-bottom: 20px;
+}
+.product-button{
+margin-left: auto;
+color: #2175f5;
+text-decoration: none;
+border: 2px solid;
+padding: 8px 24px;
+border-radius: 20px;
+transition: .4s linear;
+}
+
+.product-button:hover{
+transform: scale(1.06);
+}
+</style>
